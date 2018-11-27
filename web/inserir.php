09e94eb72114edd8adee50dc752cae788651f646
Bernardo Valente <bernardofvalente@gmail.com>
2018-11-27 22:28:11 +0000

    minor tweaks

---
+++ web/inserir.php
@@ -0,0 +1,29 @@
+<html>
+	<head>
+
+	</head>
+	<body>
+		<h3>Inserir</h3>
+		<p>Inserir e remover Locais, Eventos de Emergência, Processos de Socorro, Meios e Entidades</p>
+
+		<?php
+		try{
+
+			$host = "db.ist.utl.pt";
+			$user ="ist187521";
+			$password = "bananasplit";
+			$dbname = $user;
+
+			$db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+			$db = null;
+
+		} catch (PDOException $e){
+			echo("<p>ERROR: {$e->getMessage()}</p>");
+		}
+		?>
+
+	</body>
+</html>
