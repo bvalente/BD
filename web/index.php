b42d0e927ee61b17507856701c45ece1805149f8
Bernardo Valente <bernardofvalente@gmail.com>
2018-11-27 19:54:51 +0000

    started php

---
+++ web/index.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+<!-- http://form.guide/php-form/php-form-select.html -->
+	</head>
+	<body>
+		<h3>Aplicacao</h3>
+		<a href="inserir.php">Inserir</a>
+		<a href="remover.php">Remover</a>
+		<a href="editar.php">Editar</a>
+		<a href="associar.php">Associar</a>
+		<a href="listar.php">Listar</a>
+
+	</body>
+</html>
---
+++ web/listar.php
@@ -0,0 +1,55 @@
+<html>
+	<head>
+
+	</head>
+	<body>
+		<h3>Listar</h3>
+
+		<?php
+		try{
+
+			$host = "db.ist.utl.pt";
+			$user ="ist187521";
+			$password = "bananasplit";
+			$dbname = $user;
+
+			$db = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
+			$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+			$sql = "SELECT numprocessosocorro FROM processosocorro;";
+			$result = $db->prepare($sql);
+	        $result->execute();
+
+			echo("<table border=\"0\" cellspacing=\"5\">\n");
+			foreach($result as $row)
+			{
+				echo("<tr>\n");
+				echo("<td>{$row['numprocessosocorro']}</td>\n");
+				echo("</tr>\n");
+			}
+			echo("</table>\n");
+
+			$sql = "SELECT nummeio, nomemeio, nomeentidade FROM meio;";
+			$result = $db->prepare($sql);
+			$result->execute();
+
+			echo("<table border=\"0\" cellspacing=\"5\">\n");
+			foreach($result as $row)
+			{
+				echo("<tr>\n");
+				echo("<td>{$row['nummeio']}</td>\n");
+				echo("<td>{$row['nomemeio']}</td>\n");
+				echo("<td>{$row['nomeentidade']}</td>\n");
+				echo("</tr>\n");
+			}
+			echo("</table>\n");
+
+			$db = null;
+
+		} catch (PDOException $e){
+			echo("<p>ERROR: {$e->getMessage()}</p>");
+		}
+		?>
+
+	</body>
+</html>
